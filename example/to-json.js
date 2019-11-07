7f68562a3e48fec7234ca92dc12d62edcaa97bb9
haroldo <haroldoop@gmail.com>
2019-11-07 20:29:08 -0300

    Corrected files after moving them around.

--- example/to-json.js
+++ example/to-json.js
@@ -1,8 +1,8 @@
-const {parseWorld} = require('./index');
+const {parseWorld} = require('../');
 const {readFileSync} = require('fs');
 const stringify = require("json-stringify-pretty-compact");
 
-const source = readFileSync('minimal.bitsy', 'utf8');
+const source = readFileSync('example.bitsy', 'utf8');
 const parsedObject = parseWorld(source);
 const json = stringify(parsedObject, {maxLength: 160});
 
