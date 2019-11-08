b93e53c38639734301180ec7aeab72c449c130b7
haroldo <haroldoop@gmail.com>
2019-11-08 20:28:16 -0300

    Added unit test for version 0.0.1

--- test/bitsy.test.js
+++ test/bitsy.test.js
@@ -21,3 +21,13 @@ test('Checks if conversion of red-dog-endless-spaceship.bitsy works correctly bo
 	
   expect(serializedData).toBe(source);
 });
+
+
+test('Checks if old JSON from version 0.0.1 still gets serialized correctly.', () => {
+
+  const bitsySource = readFileSync('test/minimal.bitsy', 'utf8');
+  const source = readFileSync('test/old.0.0.1.json', 'utf8');
+  const serializedData = serializeWorld(source);
+	
+  expect(serializedData).toBe(bitsySource);
+});
