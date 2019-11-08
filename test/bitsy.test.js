597e5cc9b50a0c3e7465b599f4625e32d6eeda5e
haroldo <haroldoop@gmail.com>
2019-11-07 21:07:20 -0300

    Added one more script for testing.

--- test/bitsy.test.js
+++ test/bitsy.test.js
@@ -1,13 +1,22 @@
 
 const {parseWorld, serializeWorld} = require('../');
-
 const {readFileSync} = require('fs');
+
 
 test('Checks if conversion of minimal.bitsy works correctly both ways.', () => {
 
   const source = readFileSync('test/minimal.bitsy', 'utf8');
   const parsedData = parseWorld(source);
-  //console.log(JSON.stringify(parsedData, null, 2));
+  const serializedData = serializeWorld(parsedData);
+	
+  expect(serializedData).toBe(source);
+});
+
+
+test('Checks if conversion of red-dog-endless-spaceship.bitsy works correctly both ways.', () => {
+
+  const source = readFileSync('test/red-dog-endless-spaceship.bitsy', 'utf8');
+  const parsedData = parseWorld(source);
   const serializedData = serializeWorld(parsedData);
 	
   expect(serializedData).toBe(source);
